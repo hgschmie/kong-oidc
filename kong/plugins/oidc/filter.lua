cb7f472de6d5bbf1ab73cf4d780029be267a09cf
saroh <saroh@DESKTOP-0M1KAKK>
2020-10-31 16:00:57 +0530

    added logs

--- kong/plugins/oidc/filter.lua
+++ kong/plugins/oidc/filter.lua
@@ -3,6 +3,8 @@ local M = {}
 local function shouldIgnoreRequest(patterns)
   if (patterns) then
     for _, pattern in ipairs(patterns) do
+      ngx.log(ngx.DEBUG, "OidcHandler handling request, path: " .. ngx.var.request_uri)
+      ngx.log(ngx.DEBUG, "OidcHandler handling request, path ur: " .. ngx.var.uri)
       local isMatching = not (string.find(ngx.var.uri, pattern) == nil)
       if (isMatching) then return true end
     end
