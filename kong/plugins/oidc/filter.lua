83d73874559ebfaf2d2cf53c53d1a997f0c0d885
saroh <saroh@DESKTOP-0M1KAKK>
2020-10-31 17:50:05 +0530

    debug phase 3

--- kong/plugins/oidc/filter.lua
+++ kong/plugins/oidc/filter.lua
@@ -10,16 +10,19 @@ local function shouldIgnoreRequest(patterns)
 	    objProp[idx] = value
 	    idx = idx + 1
 	  end
-	  ngx.log(ngx.DEBUG, "OidcHandler handling request, path: " .. ngx.var.request_uri .. " host " .. ngx.var.host .. " pattern " ..  objProp[1] .. " : " .. objProp[2])
+	  ngx.log(ngx.DEBUG, "OidcHandler handling request, path: " .. ngx.var.request_uri .. " host " .. ngx.var.host .. " pattern " ..  pattern)
       local isMatching = false
       if (objProp[1]) then
       	if (objProp[2]) then
+      		ngx.log(ngx.DEBUG, "OidcHandler handling request, pattern " ..  objProp[1] .. " : " .. objProp[2])
       		isMatching = (string.find(ngx.var.uri, objProp[2]) ~= nil and ngx.var.host ~= objProp[1])
       	else
+      		ngx.log(ngx.DEBUG, "OidcHandler handling request, pattern " ..  objProp[1])
       		isMatching = (ngx.var.host ~= objProp[1])
       	end
       else
-      	if (objProp[2]) then
+      	if (objProp[2]) then      		
+      		ngx.log(ngx.DEBUG, "OidcHandler handling request, pattern  : " .. objProp[2])
       		isMatching = string.find(ngx.var.uri, objProp[2]) ~= nil
       	end
       end
