da6adfdca65a897a4a0904d4dc53c1485fe94fab
saroh <saroh@DESKTOP-0M1KAKK>
2020-10-31 17:30:12 +0530

    fixed pattern matching for host:uri

--- kong/plugins/oidc/filter.lua
+++ kong/plugins/oidc/filter.lua
@@ -6,16 +6,22 @@ local function shouldIgnoreRequest(patterns)
    local idx = 1
     for _, pattern in pairs(patterns) do
       idx = 1
-      ngx.log(ngx.DEBUG, "OidcHandler handling request, path: " .. ngx.var.request_uri .. " host " .. ngx.var.host .. " httphost " .. ngx.var.http_host )
+      ngx.log(ngx.DEBUG, "OidcHandler handling request, path: " .. ngx.var.request_uri .. " host " .. ngx.var.host )
       for value in string.gmatch(pattern, "[^:]+") do 
-	    objProp[index] = value
+	    objProp[idx] = value
 	    idx = idx + 1
 	  end
       local isMatching = false
       if (objProp[1]) then
-      	isMatching = (string.find(ngx.var.uri, objProp[2]) ~= nil and ngx.var.host ~= objProp[1])
+      	if (objProp[2]) then
+      		isMatching = (string.find(ngx.var.uri, objProp[2]) ~= nil and ngx.var.host ~= objProp[1])
+      	else
+      		isMatching = (ngx.var.host ~= objProp[1])
+      	end
       else
-      	isMatching = string.find(ngx.var.uri, objProp[2]) ~= nil
+      	if (objProp[2]) then
+      		isMatching = string.find(ngx.var.uri, objProp[2]) ~= nil
+      	else
       end
       if (isMatching) then return true end
     end
